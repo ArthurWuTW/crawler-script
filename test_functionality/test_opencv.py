53311e2da66d3a74a4567f7d79bdf2b6639932c5
ArthurWuTW <lion811004@gmail.com>
2020-09-23 01:43:21 +0100

    test opencv encode base64 and decode

--- test_functionality/test_opencv.py
+++ test_functionality/test_opencv.py
@@ -9,3 +9,20 @@ height, width, channels = frame.shape
 print(height, width, channels)
 
 print(frame)
+
+import base64
+import numpy as np
+
+base64_str = cv2.imencode('.jpg', frame)[1].tostring()
+
+print(base64_str)
+
+base64_str = base64.b64encode(base64_str)
+
+print(base64_str)
+
+imgString = base64.b64decode(base64_str)
+nparr = np.fromstring(imgString, np.uint8)
+image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+print(image)
