2a82b8d77c1e523ba73d87751f0db9a26e70660d
ArthurWuTW <lion811004@gmail.com>
2020-09-29 12:42:21 +0100

    Add relay test

---
+++ test_functionality/test_relay.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+ 
+GPIO_PIN = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+ 
+try:
+    while True:
+        print('on')
+        GPIO.output(GPIO_PIN, GPIO.HIGH)
+        time.sleep(1)
+        print('off')
+        GPIO.output(GPIO_PIN, GPIO.LOW)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print('aaa')
+finally:
+    GPIO.cleanup()
