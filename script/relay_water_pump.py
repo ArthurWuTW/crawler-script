0fcb184e959db0f3ef73ebb957da2a34dbdfcb0d
ArthurWuTW <lion811004@gmail.com>
2020-10-05 22:26:01 +0800

    Add script pump relay

---
+++ script/relay_water_pump.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+ 
+GPIO_PIN = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+ 
+try:
+    while True:
+        print('on')
+        GPIO.output(GPIO_PIN, GPIO.HIGH)
+        time.sleep(4)
+        print('off')
+        GPIO.output(GPIO_PIN, GPIO.LOW)
+        #time.sleep(5)
+        break
+except KeyboardInterrupt:
+    print('aaa')
+finally:
+    GPIO.cleanup()
