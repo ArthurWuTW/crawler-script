d6d3eb57f0a6f12f84fee601d75132b8e4d23adb
arthur <lion811004@gmail.com>
2020-10-13 10:22:36 +0800

    Temperature and Hunidity: Change another crawler webpage

--- script/temperature_humidity.py
+++ script/temperature_humidity.py
@@ -9,17 +9,14 @@ import json
 import io
 
 while True:
-  URL = "https://weather.com/zh-TW/weather/hourbyhour/l/ab6a0d440cf29997c96b86e11b647c285d3a489a623ea04d29fdefe0ea3534b2"
+  URL = "https://www.cwb.gov.tw/V8/C/W/Observe/MOD/24hr/C0V77.html"
   res = requests.get(URL)
   soup = BeautifulSoup(res.text, 'html.parser')
-  divs = soup.find_all("ul", {"class": "_2qH8C", "data-testid": "DetailsTable"})
-  divs = divs[0].find_all("li", {"class": "_3Kt-N _23W42", "data-testid": "HumiditySection"})
-  divs = divs[0].find_all("span", {"class": "_1F3Ze"})
-  humidity = divs[0].string[:-1]
+  divs = soup.find_all("td", {"headers": "hum"})
+  humidity = divs[0].string
 
-  divs = soup.find_all("summary", {"class": "AvowU _2nJx1 LEvZQ"})
-  divs = divs[0].find_all("span", {"class": "RcZzi", "data-testid": "TemperatureValue"})
-  temperature = divs[0].string[:-1]
+  divs = soup.find_all("span", {"class": "tem-C is-active"})
+  temperature = divs[0].string
 
   print(humidity, temperature)
 
