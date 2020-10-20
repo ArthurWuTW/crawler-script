3801c9a4bc4721b222c0c3b2b7403c0cfd8f5e5c
arthur <lion811004@gmail.com>
2020-10-20 19:34:37 +0800

    (yet test)Aruco detection draft

--- test_functionality/motor.py
+++ test_functionality/motor.py
@@ -5,18 +5,56 @@ In1, In2 = 6, 5
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(In1, GPIO.OUT)
 GPIO.setup(In2, GPIO.OUT)
- 
+
+def forward():
+    GPIO.output(In1, GPIO.HIGH)
+    GPIO.output(In2, GPIO.LOW)
+
+def backward():
+    GPIO.output(In2, GPIO.HIGH)
+    GPIO.output(In1, GPIO.LOW)
+
+import cv2
+from cv2 import aruco
+
+def arucoCenterDetection(image):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters =  aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    
+    center_points_dict_array = list()
+    for i in range(len(ids)):
+        c = cornors[i][0]
+
+        center_point = dict()
+        center_point['id'] = ids[i]
+        center_point['center_point'] = [c[:, 0].mean()], [c[:, 1].mean()]
+        center_points_dict_array.append(center_point)
+    
+    return center_points_dict_array
+
+
+capturing = cv2.VideoCapture(0)
+
+def get_image_frame():
+    _, frame = capturing.read()
+    return frame
+
 try:
     while True:
-        print('on')
-        GPIO.output(In1, GPIO.HIGH)
-        GPIO.output(In2, GPIO.LOW)
-        time.sleep(5)
-        print('reverse')
-        GPIO.output(In2, GPIO.HIGH)
-        GPIO.output(In1, GPIO.LOW)
-        time.sleep(5)
-        break
+
+        image = get_image_frame()
+        center_point_array = arucoCenterDetection(image)
+        print(center_point_array)
+
+        #print('forward')
+        #forward()
+        #time.sleep(5)
+        #print('backward')
+        #backward()
+        #time.sleep(5)
+        #break
 except KeyboardInterrupt:
     print('aaa')
 finally:
