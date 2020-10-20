54b9be3d4ee34e688cfbd81f6d696e1221f6166f
arthur <lion811004@gmail.com>
2020-10-20 18:31:21 +0800

    Aruco detection script draft

--- test_functionality/aruco_detection.py
+++ test_functionality/aruco_detection.py
@@ -1,2 +1,9 @@
-import cv2
+import numpy as np
+import cv2, PIL
+from cv2 import aruco
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 
