c1a8d7cff659dad563765eda2075e42042aa7a68
ArthurWuTW <lion811004@gmail.com>
2020-10-21 20:29:40 +0800

    (test complete) use Aruco in motor

--- test_functionality/motor.py
+++ test_functionality/motor.py
@@ -30,45 +30,61 @@ def arucoCenterDetection(image):
     center_points_dict_array = list()
     for i in range(len(ids)):
         c = corners[i][0]
-
-        center_point = dict()
-        center_point['id'] = ids[i][0]
-        center_point['center_point'] = [c[:, 0].mean(), c[:, 1].mean()]
-        center_points_dict_array.append(center_point)
+        #if(ids[i][0]==1):
+        #    print(c)
+        if(len(c[:, 0])==4):
+            center_point = dict()
+            center_point['id'] = ids[i][0]
+            center_point['center_point'] = [c[:, 0].mean(), c[:, 1].mean()]
+            center_points_dict_array.append(center_point)
     
     return center_points_dict_array
 
 
-capturing = cv2.VideoCapture(0)
+#capturing = cv2.VideoCapture(0)
 initial_gpio()
 
 def get_image_frame():
     _, frame = capturing.read()
     return frame
 
+count = 0
+
 try:
     while True:
 
+        capturing = None
+
+        capturing = cv2.VideoCapture(0)
+
         image = get_image_frame()
-        print(image.shape)
+        #print(image.shape)
         center_point_array = arucoCenterDetection(image)
-        print(center_point_array)
+        #print(center_point_array)
 
         for center_points in center_point_array:
             if(center_points['id'] == 1):
                 print("id is 1!")
 
-                if(center_points['center_point'][0] < image.shape[1]):
+                if(center_points['center_point'][0] < 320):
                     print("left side!")
+                    print(center_points['center_point'])
+                    backward()
+                    time.sleep(0.5)
+                    initial_gpio()
+                else:
+                    print("right side")
+                    print(center_points['center_point'])
                     forward()
-                    time.sleep(2)
+                    time.sleep(0.5)
+                    initial_gpio()
+                break
         #print('forward')
         #forward()
         #time.sleep(5)
         #print('backward')
         #backward()
         #time.sleep(5)
-        break
 except KeyboardInterrupt:
     print('aaa')
 finally:
