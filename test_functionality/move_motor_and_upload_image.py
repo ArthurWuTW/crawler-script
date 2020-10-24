d270b1fa2c311d2d6218b3ebfac5219205f623bc
ArthurWuTW <lion811004@gmail.com>
2020-10-24 14:43:41 +0800

    Move Motor test

--- test_functionality/move_motor_and_upload_image.py
+++ test_functionality/move_motor_and_upload_image.py
@@ -16,30 +16,31 @@ class Motor():
         self.gpio.setup(self.input2, GPIO.OUT)
         self.maxStep = 100
 
-    def stop():
+    def stop(self):
         self.gpio.output(self.input1, GPIO.LOW)
         self.gpio.output(self.input2, GPIO.LOW)
 
-    def forward(second):
+    def forward(self, second):
         self.gpio.output(self.input1, GPIO.HIGH)
         self.gpio.output(self.input2, GPIO.LOW)
         time.sleep(second)
-        stop()
+        self.stop()
 
-    def backward(second):
+    def backward(self, second):
         self.gpio.output(self.input2, GPIO.HIGH)
         self.gpio.output(self.input1, GPIO.LOW)
         time.sleep(second)
-        stop()
+        self.stop()
 
 class ImageGrabber():
     def __init__(self, id):
         self.grabber = cv2.VideoCapture(id)
 
-    def resetCaputringConfiguration(id):
+    def resetCaputringConfiguration(self, id):
+        self.grabber = None
         self.grabber = cv2.VideoCapture(id)
 
-    def getImageFrame():
+    def getImageFrame(self):
         _, frame = self.grabber.read()
         return frame
 
@@ -47,34 +48,34 @@ class ArucoLibrary():
     def __init__(self):
         self.ARUCO = aruco
 
-    def detectArucoCorner(image):
+    def detectArucoCorner(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         aruco_dict = self.ARUCO.Dictionary_get(aruco.DICT_6X6_250)
         parameters =  self.ARUCO.DetectorParameters_create()
         corners, ids, _ = self.ARUCO.detectMarkers(gray, aruco_dict, parameters=parameters)
         return corners, ids
 
-    def getCenterPoints(corners, ids):
+    def getCenterPoints(self, corners, ids):
         centerPoints = list()
         for i in range(len(ids)):
             c = corners[i][0]
-            if(len(c[:.0]) == 4):
+            if(len(c[:, 0]) == 4):
                 centerPoint = dict()
                 centerPoint['id'] = ids[i][0]
                 centerPoint['x'] = c[:, 0].mean()
                 centerPoint['y'] = c[:, 1].mean()
-                centerPointsArray.append(centerPoint)
+                centerPoints.append(centerPoint)
         return centerPoints
 
-def ImagePoster():
+class ImagePoster():
     def __init__(self, url):
         self.url = url
-    def postImageArucoid(image, id):
+    def postImageArucoid(self, image, id):
         base64_str = cv2.imencode('.jpg', image)[1].tostring()
         base64_str = base64.b64encode(base64_str)
         base64_str_decod_utf_8 = base64_str.decode("utf-8")
         data = {
-            'id': id
+            'id': id,
             'image': base64_str_decod_utf_8,
         }
         headers = {'content-type': 'application/json'}
@@ -85,9 +86,10 @@ if __name__ == '__main__':
     motor.stop()
     imageGrabber = ImageGrabber(0)
     arucoLib = ArucoLibrary()
-    imagePoster = ImagePoster('http://10.1.1.16:8000/receiveImage')
+    ip = "http://10.1.1.16:8000/receiveImage"
+    imagePoster = ImagePoster(ip)
 
-    arucoIdArray = [6, 5, 4, 3, 2, 1]
+    arucoIdArray = [1, 2, 3, 4, 5, 6]
     try:
         for id in arucoIdArray:
             moveCount = 0
@@ -111,7 +113,7 @@ if __name__ == '__main__':
                 if(moveCount == 100):
                     imagePoster.postImageArucoid(image, id)
                     break
-        except KeyboardInterrupt:
-            print('KeyboardInterrupt')
-        finally:
-            motor.gpio.cleanup()
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt')
+    finally:
+        motor.gpio.cleanup()
