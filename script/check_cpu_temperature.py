4bcf86706cd822c319075b4cf2595dbb3354868c
ArthurWuTW <lion811004@gmail.com>
2020-11-09 20:16:18 +0800

    check cpu add pass status to message log

--- script/check_cpu_temperature.py
+++ script/check_cpu_temperature.py
@@ -22,6 +22,7 @@ while True:
 
     try:
         r = requests.get(ip+data)
+        r = requests.get(ip+"/writeLogMessage/CPU/%5BLOG%5D%20CPU%20Data%20Updated")
     except:
         pass
     break
